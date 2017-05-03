01f649112046300c61818ac51a8bd4804ae163d3
Wilson Tovar <twilson@uninorte.edu.co>
2017-05-03 13:26:44 -0500

    Controlador colonia

--- app/controllers/colony_controller.js
+++ app/controllers/colony_controller.js
@@ -1,3 +1,7 @@
+/**
+ * @description Controlador - Colonias
+ */
+
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
@@ -12,14 +16,14 @@ module.exports = function(app) {
 /* ADMIN - Ver todas las colonias */
 // TODO: Implementar
 router.get('/admin/colonies', function(req, res, next) {
-  console.log('Método sin implementar...');
-  res.end();
+    console.log('Método sin implementar...');
+    res.end();
 });
 
 
 /* ADMIN POST Crear colonia */
 // TODO: Implementar
 router.post('/admin/colonies/new', function(req, res) {
-  console.log('Método sin implementar...');
-  res.end();
+    console.log('Método sin implementar...');
+    res.end();
 });
