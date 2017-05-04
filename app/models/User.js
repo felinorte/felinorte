7a92bd8b861f18c3a429b13650791abd99ba5d15
twilson <twilson@uninorte.edu.co>
2017-05-04 15:48:19 -0400

    Panel de administracion

--- app/models/User.js
+++ app/models/User.js
@@ -3,9 +3,10 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;// Creando object Schema
 var UserSchema= new Schema({
   mail: String,
-  pass: string,
+  pass: String,
   first_name: String,
   last_name: String,
+  cargo: String,
 });
 
 mongoose.model('User', UserSchema);
