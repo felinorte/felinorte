01aa386ccecd558d64471fa269c8df5fb77f2331
twilson <twilson@uninorte.edu.co>
2017-05-03 22:05:34 -0400

    Dashboard

--- app/controllers/admin_controller.js
+++ app/controllers/admin_controller.js
@@ -2,11 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(app) {
-    app.use('/', router);
+  app.use('/', router);
 };
 
 /* GET Login del panel de administración */
 // TODO: Verificar que el admin no esté logueado
 router.get('/admin/login', function(req, res, next) {
   res.render('admin/login');
+});
+
+/* GET Dashboard de administración */
+router.get('/admin', function(req, res, next) {
+  res.render('admin/index');
+});
+
+/* GET Ver todos los gatos */
+router.get('/admin/gatos', function(req, res, next) {
+  res.render('admin/gatos');
 });
