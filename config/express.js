39894919cd9841416251cecc82b9a114a0ffb4dc
twilson <twilson@uninorte.edu.co>
2017-05-04 22:30:17 -0400

    Prueba 2

--- config/express.js
+++ config/express.js
@@ -9,54 +9,58 @@ var compress = require('compression');
 var methodOverride = require('method-override');
 
 module.exports = function(app, config) {
-    var env = process.env.NODE_ENV || 'development';
-    app.locals.ENV = env;
-    app.locals.ENV_DEVELOPMENT = env == 'development';
+  var env = process.env.NODE_ENV || 'development';
+  app.locals.ENV = env;
+  app.locals.ENV_DEVELOPMENT = env == 'development';
 
-    app.set('views', config.root + '/app/views');
-    app.set('view engine', 'pug');
+  var path = require('path');
 
-    // app.use(favicon(config.root + '/public/img/favicon.ico'));
-    app.use(logger('dev'));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(cookieParser());
-    app.use(compress());
-    app.use(express.static(config.root + '/public'));
-    app.use(methodOverride());
+  app.set('views', path.join(process.cwd() + '/views'));
+  app.use(express.static(path.join(process.cwd() + '/public')));
+  app.set('views', config.root + '/app/views');
+  app.set('view engine', 'pug');
 
-    var controllers = glob.sync(config.root + '/app/controllers/*.js');
-    controllers.forEach(function(controller) {
-        require(controller)(app);
-    });
+  // app.use(favicon(config.root + '/public/img/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(cookieParser());
+  app.use(compress());
+  // app.use(express.static(config.root + '/public'));
+  app.use(methodOverride());
 
-    app.use(function(req, res, next) {
-        var err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-    });
+  var controllers = glob.sync(config.root + '/app/controllers/*.js');
+  controllers.forEach(function(controller) {
+    require(controller)(app);
+  });
 
-    if (app.get('env') === 'development') {
-        app.use(function(err, req, res, next) {
-            res.status(err.status || 500);
-            res.render('error', {
-                message: err.message,
-                error: err,
-                title: 'error'
-            });
-        });
-    }
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
+  if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {},
-            title: 'error'
-        });
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err,
+        title: 'error'
+      });
     });
+  }
 
-    return app;
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {},
+      title: 'error'
+    });
+  });
+
+  return app;
 };
